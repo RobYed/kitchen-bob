fe49e0781951c2c4afeded9fbe746cdbf6180fdc
Robert Dey <robdey88@gmail.com>
2015-02-20 23:30:26 +0100

    updated template content

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,7 +1,6 @@
 angular.module('starter.controllers', [])
 
-
-.controller('HomeCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+.controller('NavCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
   
   $scope.viewTitle = "Kitchen Bob";
   
@@ -11,28 +10,32 @@ angular.module('starter.controllers', [])
 
 }])
 
+.controller('HomeCtrl', function($scope, $mdSidenav) {
+  $scope.viewTitle = "Kitchen Bob";
+})
+
 .controller('RecipeAddCtrl', function($scope) {
   $scope.viewTitle = "Add a new recipe";
 })
 
 .controller('RecipeAddIngredientsCtrl', function($scope) {
-  
+  $scope.viewTitle = "Add a new recipe";
 })
 
 .controller('RecipeAddPreparationCtrl', function($scope) {
-  
+  $scope.viewTitle = "Add a new recipe";
 })
 
 .controller('RecipeDetailCtrl', function($scope) {
-  
+  $scope.viewTitle = "Add a new recipe";
 })
 
 .controller('RecipeShoppinglistCtrl', function($scope) {
-  
+  $scope.viewTitle = "Add a new recipe";
 })
 
 .controller('RecipePreparationCtrl', function($scope) {
-  
+  $scope.viewTitle = "Add a new recipe";
 });
 
 
